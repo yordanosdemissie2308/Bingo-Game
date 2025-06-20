510ccce0d867ca3843e1cef3a2b4e544b3e5bf8f
Yordanos Demissie <yordanosellwork@gmail.com>
2025-06-20 09:44:27 +0300

    last comiit on page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import LoginPage from "../layout/aut/Login";
 
-export const Login = () => {
+export default function Login() {
   return <LoginPage />;
-};
-
-export default Login;
+}
